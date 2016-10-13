239cb1a636fb54b0b43df6a522d63babe2e209e6
xfannax <fanta_ik@hotmail.com>
2016-10-13 16:39:17 +0200

    part of 2b.3

--- lab2/2b.hs
+++ lab2/2b.hs
@@ -39,3 +39,38 @@ uniq (x:xs) = sortList(x:uniq(filter (/= x) xs))
           sortList (x1:x2:xs)
               | x1 < x2 = x1 : sortList (x2:xs)
               | otherwise = x2 : sortList (x1:xs)
+
+
+type Valuation = [(Name, Integer)]
+
+evalExpr :: Expr -> Valuation -> Integer
+evalExpr e v
+    | (verify e v) == True = evalExpr e v
+    | otherwise            = error
+evalExpr (Var x) v = sure(lookup x v) where sure (Just b) = b
+evalExpr (Val y) v = y
+evalExpr (p :+: q) v = evalExpr p v + (evalExpr q v)
+evalExpr (p :-: q) v = evalExpr p v - (evalExpr q v)
+evalExpr (p :*: q) v = evalExpr p v * (evalExpr q v)
+evalExpr (p :/: q) v = evalExpr p v `div` (evalExpr q v)
+evalExpr (p :%: q) v = evalExpr p v `mod` (evalExpr q v)
+
+
+-- empty valuation eventhough given variable: (Var "a") [] or (Var "a" + Var "b") [("a", 2)]
+-- declared as Var but not of the type variable: (Var 2)
+-- variable name has other than number in m: (Var "a1")
+-- empty Val: (Val :+: Val)
+-- declared as Val but not of type value: (Val "a")
+verify :: Expr -> Valuation -> Bool
+verify (Var x) [] = False
+verify (Var x) _  = checkIt x
+verify (Val y) _ = checkIt [y]
+verify (p :+: q) v = verify p v && verify q v
+verify (p :-: q) v = verify p v && verify q v
+verify (p :*: q) v = verify p v && verify q v
+verify (p :/: q) v = verify p v && verify q v
+verify (p :%: q) v = verify p v && verify q v
+
+checkIt :: [a] -> Bool
+checkIt [] = False
+checkIt _  = True
