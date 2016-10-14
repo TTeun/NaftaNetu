6aa302152dbee3630e40ee4c1cd66a6bb0de95b0
xfannax <fanta_ik@hotmail.com>
2016-10-14 12:23:18 +0200

    2b: correct part 1-3

--- lab2/2b.hs
+++ lab2/2b.hs
@@ -13,8 +13,8 @@ par :: String -> String
 par s = "(" ++ s ++ ")"
 
 instance Show Expr where
-    show (Var x) = x
-    show (Val y) = show y
+    show (Var x)   = x
+    show (Val y)   = show y
     show (p :+: q) = par(show p ++ "+" ++ show q)
     show (p :-: q) = par(show p ++ "-" ++ show q)
     show (p :*: q) = par(show p ++ "*" ++ show q)
@@ -22,8 +22,8 @@ instance Show Expr where
     show (p :%: q) = par(show p ++ "%" ++ show q)
 
 vars :: Expr -> [Name]
-vars (Var x) = [x]
-vars (Val y) = []
+vars (Var x)   = [x]
+vars (Val y)   = []
 vars (p :+: q) = uniq(vars p ++ vars q)
 vars (p :-: q) = uniq(vars p ++ vars q)
 vars (p :*: q) = uniq(vars p ++ vars q)
@@ -31,46 +31,28 @@ vars (p :/: q) = uniq(vars p ++ vars q)
 vars (p :%: q) = uniq(vars p ++ vars q)
 
 uniq :: Ord a => [a] -> [a]
-uniq [] = []
+uniq []     = []
 uniq (x:xs) = sortList(x:uniq(filter (/= x) xs))
     where sortList :: Ord a => [a] -> [a]
-          sortList [] = []
-          sortList (x:[]) = [x]
+          sortList []          = []
+          sortList (x:[])      = [x]
           sortList (x1:x2:xs)
-              | x1 < x2 = x1 : sortList (x2:xs)
-              | otherwise = x2 : sortList (x1:xs)
-
+              | x1 < x2        = x1 : sortList (x2:xs)
+              | otherwise      = x2 : sortList (x1:xs)
 
 type Valuation = [(Name, Integer)]
 
 evalExpr :: Expr -> Valuation -> Integer
-evalExpr e v
-    | (verify e v) == True = evalExpr e v
-    | otherwise            = error
-evalExpr (Var x) v = sure(lookup x v) where sure (Just b) = b
-evalExpr (Val y) v = y
+evalExpr (Var x) v   = verify (lookup x v)
+evalExpr (Val y) v   = y
 evalExpr (p :+: q) v = evalExpr p v + (evalExpr q v)
 evalExpr (p :-: q) v = evalExpr p v - (evalExpr q v)
 evalExpr (p :*: q) v = evalExpr p v * (evalExpr q v)
 evalExpr (p :/: q) v = evalExpr p v `div` (evalExpr q v)
 evalExpr (p :%: q) v = evalExpr p v `mod` (evalExpr q v)
 
-
--- empty valuation eventhough given variable: (Var "a") [] or (Var "a" + Var "b") [("a", 2)]
--- declared as Var but not of the type variable: (Var 2)
--- variable name has other than number in m: (Var "a1")
--- empty Val: (Val :+: Val)
--- declared as Val but not of type value: (Val "a")
-verify :: Expr -> Valuation -> Bool
-verify (Var x) [] = False
-verify (Var x) _  = checkIt x
-verify (Val y) _ = checkIt [y]
-verify (p :+: q) v = verify p v && verify q v
-verify (p :-: q) v = verify p v && verify q v
-verify (p :*: q) v = verify p v && verify q v
-verify (p :/: q) v = verify p v && verify q v
-verify (p :%: q) v = verify p v && verify q v
-
-checkIt :: [a] -> Bool
-checkIt [] = False
-checkIt _  = True
+verify :: Maybe Integer -> Integer
+verify b
+        | b == Nothing = error "Valuation is not complete"
+        | otherwise    = sure b
+            where sure (Just b) = b
